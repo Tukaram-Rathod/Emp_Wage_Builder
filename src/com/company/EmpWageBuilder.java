8fc96ff18fa8d05aa20dd01d0aeb818da4f67dfd
Tukaram-Rathod <tukaram.p.rathod02@gmail.com>
2021-04-02 09:34:29 +0530

    Added. calculate daily wages add part time

--- src/com/company/EmpWageBuilder.java
+++ src/com/company/EmpWageBuilder.java
@@ -4,26 +4,22 @@ public class EmpWageBuilder {
     public static void main(String[] args) {
         System.out.println("Welcome To EmpWageBuilder ");
         //constants
-        int IS_FULL_TIME = 1;
+        int IS_PART_TIME = 1;
+        int IS_FULL_TIME = 2;
         int EMP_RATE_PER_HOUR = 20;
-
         //Variable
         int empHrs = 0;
         int empWage = 0;
-
         //computation
-        double empCheck = Math.floor(Math.random() * 10 ) % 2;
+        double empCheck = Math.floor(Math.random() * 10 ) % 3;
         if (empCheck == IS_FULL_TIME)
             empHrs = 8;
-
+        else if (empCheck == IS_PART_TIME)
+            empHrs = 4;
         else
             empHrs = 0;
-
         //calculate daily wages
-
         empWage = empHrs *  EMP_RATE_PER_HOUR;
         System.out.println("Emp WAGE " +empWage );
-
     }
-
 }
