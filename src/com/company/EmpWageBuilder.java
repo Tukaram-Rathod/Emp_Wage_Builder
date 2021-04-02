fba32299f14844b5cd616f0620760e634843529a
Tukaram-Rathod <tukaram.p.rathod02@gmail.com>
2021-04-02 09:50:21 +0530

    Added. caculate wages till a condition of total working hours or days is reacheed for a month

--- src/com/company/EmpWageBuilder.java
+++ src/com/company/EmpWageBuilder.java
@@ -6,28 +6,30 @@ public class EmpWageBuilder {
     public static final int IS_FULL_TIME = 2;
     public static final int EMP_RATE_PER_HOUR = 20;
     public static final int NUM_OF_WORKING_DAYS = 20;
+    public static final int MAX_HRS_IN_MONTH = 100;
     public static void main(String[] args) {
         System.out.println("welcome to employye wage builder program:");
         //Variable
-        int empHrs = 0, empWage = 0, totalEmpWage = 0;
+        int empHrs = 0, empWage=0,totalEmpHrs = 0,totalEmpWage=0,totalWorkingdays =0;
         //computation
-        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-        for (int day = 0; day < NUM_OF_WORKING_DAYS; day++) {
-            switch (empCheck) {
-                case IS_PART_TIME:
+        while(totalEmpHrs <= MAX_HRS_IN_MONTH &&
+                totalWorkingdays < NUM_OF_WORKING_DAYS) {
+            totalWorkingdays++;
+            int empCheck =(int) Math.floor(Math.random() * 10 ) % 3;
+            switch(empCheck) {
+                case IS_PART_TIME :
                     empHrs = 4;
                     break;
-                case IS_FULL_TIME:
+                case IS_FULL_TIME :
                     empHrs = 8;
                     break;
-                default:
+                default :
                     empHrs = 0;
             }
-            //calculate daily wages
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            totalEmpWage += empWage;
-            System.out.println("Emp WAGE ==> " + empWage);
+            totalEmpHrs += empHrs;
+            System.out.println("Days#: " +totalWorkingdays+ "Emp Hrs :" + empHrs +":" );
         }
-        System.out.println("Total EmpWage ==> " + totalEmpWage);
+        totalEmpWage = totalEmpHrs *  EMP_RATE_PER_HOUR;
+        System.out.println("Total EmpWage ==> " +totalEmpWage);
     }
 }
