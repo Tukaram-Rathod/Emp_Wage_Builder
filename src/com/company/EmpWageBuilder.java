efbdc06e2e73a887efb00f00bd77c335d7f31f9e
Tukaram-Rathod <tukaram.p.rathod02@gmail.com>
2021-04-02 09:30:47 +0530

    Added. calculate daily wages

--- src/com/company/EmpWageBuilder.java
+++ src/com/company/EmpWageBuilder.java
@@ -3,12 +3,27 @@ package com.company;
 public class EmpWageBuilder {
     public static void main(String[] args) {
         System.out.println("Welcome To EmpWageBuilder ");
-        int IS_PRESENT = 1;
+        //constants
+        int IS_FULL_TIME = 1;
+        int EMP_RATE_PER_HOUR = 20;
+
+        //Variable
+        int empHrs = 0;
+        int empWage = 0;
+
+        //computation
         double empCheck = Math.floor(Math.random() * 10 ) % 2;
-        if (empCheck == IS_PRESENT)
-            System.out.println("Employee Is Present.");
+        if (empCheck == IS_FULL_TIME)
+            empHrs = 8;
+
         else
-            System.out.println("Employee Is Absent.");
+            empHrs = 0;
+
+        //calculate daily wages
+
+        empWage = empHrs *  EMP_RATE_PER_HOUR;
+        System.out.println("Emp WAGE " +empWage );
+
     }
 
 }
