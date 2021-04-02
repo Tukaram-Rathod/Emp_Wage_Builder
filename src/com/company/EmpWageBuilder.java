514b9cf7531e90a76966b5e4dbe6ed7a41a80043
Tukaram-Rathod <tukaram.p.rathod02@gmail.com>
2021-04-02 09:18:48 +0530

    ADDED.check employee is present or absent

--- src/com/company/EmpWageBuilder.java
+++ src/com/company/EmpWageBuilder.java
@@ -3,6 +3,12 @@ package com.company;
 public class EmpWageBuilder {
     public static void main(String[] args) {
         System.out.println("Welcome To EmpWageBuilder ");
+        int IS_PRESENT = 1;
+        double empCheck = Math.floor(Math.random() * 10 ) % 2;
+        if (empCheck == IS_PRESENT)
+            System.out.println("Employee Is Present");
+        else
+            System.out.println("Employee Is Absent");
     }
 
 }
